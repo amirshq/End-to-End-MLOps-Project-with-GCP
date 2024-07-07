664572a132e426d54e5bbabfbddd5d91d6e734a4
Amir <amir.shahcheraghian@gmail.com>
2024-07-07 11:10:12 -0400

    finish main file

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-import os 
+
+import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import io
 import tensorflow as tf
-from tensorflow import keras    
+from tensorflow import keras
 import numpy as np
+
+
+model = keras.models.load_model("../nn.h5")
+
+# load the image
 from PIL import Image
-from flask import Flask, request, jsonify
+import io
+with open('three.png', 'rb') as file:
+    image_bytes = file.read()
+    pillow_img = Image.open(io.BytesIO(image_bytes)).convert('L')
 
-model = keras.models.load_model('nn.h5')
 
-def transform_image(pillow_image):
-    data = np.asarray(pillow_image)
-    data = data / 255.0
-    data = data[np.newaxis, ..., np.newaxis]
-    data = tf.image.resize(data, [28, 28])
-    return data
+# transform image, same as for training!
+data = np.asarray(pillow_img)
+data = data / 255.0
+data = data[np.newaxis, ..., np.newaxis]
+# --> [1, x, y, 1]
+data = tf.image.resize(data, [28, 28])
 
-def predict(image):
-    predictions = model(x)
-    predictions = tf.nn.softmax(predictions)
-    pred0 = predictions[0]
-    label0 = np.argmax(pred0)
-    return label0
 
-app = Flask(__name__)
-@app.route("/",methods=["GET","POST"])
+# predict
+predictions = model(data)
+predictions = tf.nn.softmax(predictions)
+pred0 = predictions[0]
+label0 = np.argmax(pred0)
+print(label0)
