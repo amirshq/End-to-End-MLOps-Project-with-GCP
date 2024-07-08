658313080e8e92d3507fb86fb5b1fbc87ce23263
Amir <amir.shahcheraghian@gmail.com>
2024-07-08 09:07:57 -0400

    update

--- test/test.py
+++ test/test.py
@@ -1,5 +1,9 @@
+
+
 import requests
 
-resp = requests.post("http://localhost:5000/", files={'file': open('eight.png', 'rb')})
+file_path = r'D:\End to End Mlops using GCP\End-to-End-MLOps-Project-with-GCP\eight.png'
+with open(file_path, 'rb') as file:
+    resp = requests.post("http://localhost:5000/", files={'file': file})
 
 print(resp.json())
