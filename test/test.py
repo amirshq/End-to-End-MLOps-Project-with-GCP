2e131370d0b4bf89ad7117b03b849dc9688089d2
Amir <amir.shahcheraghian@gmail.com>
2024-07-16 13:19:03 -0400

    update

--- test/test.py
+++ test/test.py
@@ -3,6 +3,6 @@ import requests
 
 file_path = r'D:\End to End Mlops using GCP\End-to-End-MLOps-Project-with-GCP\three.png'
 with open(file_path, 'rb') as file:
-    resp = requests.post("http://localhost:5000/", files={'file': file})
+    resp = requests.post(" https://getprediction-ytxqog5dha-ue.a.run.app", files={'file': file})
 
 print(resp.json())
